1498c5a55451c71eb36c7a16a3b5c0b3877437a0
Kava Developers <mehul9921@gmail.com>
2021-07-02 12:18:54 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -22,21 +22,22 @@ Route::get('/{slug}', 'HomeController@page');
 
 
 //Admin Routes
-Route::get('admin', 'admin\LoginController@index');
-Route::get('admin/login', 'admin\LoginController@index');
-Route::get('admin/logout', 'admin\LoginController@logout');
+Route::get('/admin', 'admin\LoginController@index');
+Route::get('/admin/login', 'admin\LoginController@index');
+Route::post('/admin/login', 'admin\LoginController@login');
+Route::get('/admin/logout', 'admin\LoginController@logout');
 
-Route::get('admin/dashboard', 'admin\DashboardController@index');
+Route::get('/admin/dashboard', 'admin\DashboardController@index');
 
 //Pages Routes
-Route::get('admin/pages', 'admin\PagesController@index');
-Route::get('admin/pages/add', 'admin\PagesController@add');
-Route::post('admin/pages/add', 'admin\PagesController@save');
-Route::get('admin/pages/edit/{id}', 'admin\PagesController@edit');
-Route::post('admin/pages/edit', 'admin\PagesController@update');
-Route::get('admin/pages/{id}', 'admin\PagesController@delete');
+Route::get('/admin/pages', 'admin\PagesController@index');
+Route::get('/admin/pages/add', 'admin\PagesController@add');
+Route::post('/admin/pages/add', 'admin\PagesController@save');
+Route::get('/admin/pages/edit/{id}', 'admin\PagesController@edit');
+Route::post('/admin/pages/edit', 'admin\PagesController@update');
+Route::get('/admin/pages/{id}', 'admin\PagesController@delete');
 
-Route::post('admin/pages/addvalidate', 'admin\PagesController@addvalidate');
+Route::post('/admin/pages/addvalidate', 'admin\PagesController@addvalidate');
 //Pages Routes
 
 
